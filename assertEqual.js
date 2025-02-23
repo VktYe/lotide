8ad7124060a96bfc5cce07d3429fb8fe70dceb3f
VktYe <v.yehorenko@gmail.com>
2025-02-22 20:52:52 -0400

    Created a function that asserts two values

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+
+//called the function, but while it is empty it does nothing
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual(36, 35);
+assertEqual("This day", "This day");
+
+
