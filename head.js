a20ffa1ac63d3fc2eacb76d530ee9bb316315080
VktYe <v.yehorenko@gmail.com>
2025-02-22 22:43:51 -0400

    Added comments to tests

--- head.js
+++ head.js
@@ -16,6 +16,6 @@ const head = function(array) {
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-assertEqual(head([1]), 5);
-assertEqual(head([]), 5);
-assertEqual(head([1]), "1");
+assertEqual(head([1]), 5); // should faild
+assertEqual(head([]), 5); // should faild
+assertEqual(head([1]), "1"); // should faild
