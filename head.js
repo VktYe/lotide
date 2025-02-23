246d81dfa285f0777a6018c8fa9ddb8ef1f4a987
VktYe <v.yehorenko@gmail.com>
2025-02-22 22:39:01 -0400

    Fixed mistakes

--- head.js
+++ head.js
@@ -6,16 +6,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 /* Create a function head which returns the first item in the array.
- - head function should not return the first element as an array. 
+ - head function should not return the first element as an array.
  - It should simply return the element itself. */
 
-// .slice(0, 1) doesn't work - doesn't return the element itself
-
-// .shift() removes the first element and returns it (not as an array)
-const head = function(value1, value2) {
-  for (const element of value1) {
-    return value1.shift();
-  }
+const head = function(array) {
+  return array[0];
 };
 
 assertEqual(head([5, 6, 7]), 5);
