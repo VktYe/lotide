4926dd4998d727283258a544628afe05d6102b55
VktYe <v.yehorenko@gmail.com>
2025-03-01 00:04:31 -0400

    added comments

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -9,10 +9,12 @@ const assertEqual = function(actual, expected) {
 // - takes an obj and a value
 // - scan the object for given value
 // - returns key with the same value or if not - undefined
+
 const findKeyByValue = function(object, value) {
+// Loop through the keys of the object
   for (const key of Object.keys(object)) {
+    // If the current key's value matches the input value, return the key
     if (object[key] === value) {
-      
       return key;
     }
   
