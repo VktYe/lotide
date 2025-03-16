b13665acaad42e0daacafaee609819e2a11dd31a
VktYe <v.yehorenko@gmail.com>
2025-03-16 20:00:28 -0300

    Added comment to a test case

--- test/headTest.js
+++ test/headTest.js
@@ -4,6 +4,6 @@ const head = require("../head");
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-assertEqual(head([1]), 5); // should faild
-assertEqual(head([]), 5); // should faild
-assertEqual(head([1]), "1"); // should faild
+assertEqual(head([1]), 5); // Testing if the head of the array [1] is 5
+assertEqual(head([]), 5); // 
+assertEqual(head([1]), "1"); // 
