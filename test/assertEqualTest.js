a191085b309a99e7617c587106636f89aae68571
VktYe <v.yehorenko@gmail.com>
2025-03-16 19:56:26 -0300

    created test files to separate test code from the function

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require("../assertEqual");
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual(36, 35);
+assertEqual("This day", "This day");
---
+++ test/headTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require("../assertEqual");
+const head = require("../head");
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+assertEqual(head([1]), 5); // should faild
+assertEqual(head([]), 5); // should faild
+assertEqual(head([1]), "1"); // should faild
