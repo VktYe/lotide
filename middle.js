fdce1dd54e9ffedf42327d3cce6d7b57626236c2
VktYe <v.yehorenko@gmail.com>
2025-03-16 21:43:30 -0300

    refactored function. removed middleOfArray variable

--- middle.js
+++ middle.js
@@ -1,20 +1,17 @@
 // Implement middle which will take in an array and return the middle-most element(s) of the given array.
 
 const middle = function(array) {
-  const middleOfArray = [];
+  // const middleOfArray = [];
   const middleIndex = Math.floor(array.length / 2); // calculating middle index number of the array
   if (array.length <= 2) {
     return [];
 
   } else if (array.length % 2 === 1) {
-    middleOfArray.push(array[middleIndex]);
+    return [array[middleIndex]];
 
   } else {
-    middleOfArray.push(array[middleIndex - 1]);
-    middleOfArray.push(array[middleIndex]);
+    return [array[middleIndex - 1], array[middleIndex]];
   }
-
-  return middleOfArray;
 };
 
 module.exports = middle;
