f3b47611822908c01e95052ad5cc6f5089c24fb7
VktYe <v.yehorenko@gmail.com>
2025-03-16 21:47:12 -0300

    removed unnecessary comment

--- middle.js
+++ middle.js
@@ -1,7 +1,7 @@
 // Implement middle which will take in an array and return the middle-most element(s) of the given array.
 
 const middle = function(array) {
-  // const middleOfArray = [];
+  
   const middleIndex = Math.floor(array.length / 2); // calculating middle index number of the array
   if (array.length <= 2) {
     return [];
