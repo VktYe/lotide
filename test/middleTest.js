e0d3690d4c456b9bf0bc32335d72af10c6517b62
VktYe <v.yehorenko@gmail.com>
2025-03-16 21:06:26 -0300

    created test files to separate test code from the function

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require("../middle");
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([10, 20, 30, 40, 50, 60]), [30, 40]);
